ea35e91a4850087dcaeefe31dbb766799fa11d51
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:02:03 -0500

    TUnit conversion

--- test/Soenneker.Extensions.Spans.Readonly.Bytes.Tests/ReadOnlySpanByteExtensionTests.cs
+++ test/Soenneker.Extensions.Spans.Readonly.Bytes.Tests/ReadOnlySpanByteExtensionTests.cs
@@ -1,11 +1,10 @@
-﻿using Soenneker.Tests.Unit;
-using Xunit;
+using Soenneker.Tests.Unit;
 
 namespace Soenneker.Extensions.Spans.Readonly.Bytes.Tests;
 
 public sealed class ReadOnlySpanByteExtensionTests : UnitTest
 {
-    [Fact]
+    [Test]
     public void Default()
     {
 
